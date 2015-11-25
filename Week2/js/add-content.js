57d59fa6cc1939528c9d45800ed2a57836eb2fa6
Viacheslav Litvinov <viacheslav.litvinov@students.stamford.edu>
2015-11-25 09:43:10 +0700

    Shopping Canter v1.1

--- Week2/js/add-content.js
+++ Week2/js/add-content.js
@@ -1,12 +1,40 @@
-var price;
-var quantity;
-var total;
+//GREETING PART
 
-price = 5;
-quantity = 14;
-total = price * quantity;
+var time = new Date().getHours();
+
+var myName = document.getElementById("name");
+myName.textContent = "Viacheslav";
+
+var greetings = document.getElementById("greeting");
+
+if (time >= 6 && time < 12) {
+	greetings.textContent = "Good morning";
+}
+else if (time >= 12 && time < 18) {
+	greetings.textContent = "Good afternoon";
+}
+else if (time >= 18 && time < 23) {
+	greetings.textContent = "Good evening"
+}
+else {
+	greetings.textContent = "Good night"
+}
 
 
-//document.write("Total is " + total);
+//PRODUCT PART
+
+var products = ["Stamford T-shirt", "Stamford Notebook", "Stamford Wristband"];
+var price = [10, 5, 1];
+var total = 0;
+var 
+
+
+var productList = document.getElementById("productList");
+for (var i = 0; i < products.length; i++) {
+	productList.innerHTML += "<li>" + products[i] + ": $" + price[i] + "</li>";
+	total += price[i];
+
+}
+
 var totalPriceEle = document.getElementById("totalPrice");
 totalPriceEle.textContent = "$" + total;
