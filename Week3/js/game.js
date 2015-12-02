ac6c866e8220bcb1bcbeb1b88064a6e286e8a462
Viacheslav Litvinov <viacheslav.litvinov@students.stamford.edu>
2015-12-02 10:20:31 +0700

    Add Week3 Task #4

--- Week3/js/game.js
+++ Week3/js/game.js
@@ -14,17 +14,81 @@ var game = {
 			self.click(2);
 		}, false);
 
+		// Number 3
+		var ele = document.getElementById("three");
+		ele.addEventListener("click", function(){
+			self.click(3);
+		}, false);
+
+		// Number 4
+		var ele = document.getElementById("four");
+		ele.addEventListener("click", function(){
+			self.click(4);
+		}, false)
+		
+		// Number 5
+		var ele = document.getElementById("five");
+		ele.addEventListener("click", function(){
+			self.click(5);
+		}, false)
+		
+		// Number 6
+		var ele = document.getElementById("six");
+		ele.addEventListener("click", function(){
+			self.click(6);
+		}, false)
+		
+		// Number 7
+		var ele = document.getElementById("seven");
+		ele.addEventListener("click", function(){
+			self.click(7);
+		}, false)
+		
+		// Number 8
+		var ele = document.getElementById("eight");
+		ele.addEventListener("click", function(){
+			self.click(8);
+		}, false)
+		
+		// Number 9
+		var ele = document.getElementById("nine");
+		ele.addEventListener("click", function(){
+			self.click(9);
+		}, false)
+		
+		// Number 0
+		var ele = document.getElementById("null");
+		ele.addEventListener("click", function(){
+			self.click(0);
+		}, false)
+
 		// Guess!
 		var ele = document.getElementById("guess");
-		ele.addEventListener("click", this.checkAnswer, false);
+		//ele.addEventListener("click", this.checkAnswer, false);
+		ele.addEventListener("click", function(){
+			self.checkAnswer(self);
+		}, false)
 	},
 	click: function(value) {
 		console.log(value + " clicked.");
 		var userValueEle = document.getElementById("userValue");
 		userValueEle.value += value;
 	},
-	checkAnswer: function() {
-		alert("This is not correct! Try again. ");
+	checkAnswer: function(self) {
+		//alert("This is not correct! Try again. ");
+		var userValueEle = document.getElementById("userValue");
+		if (userValueEle.value == self.answer) {
+			alert("Congratulations! You win!");
+			
+		}
+		else if (userValueEle.value > this.answer) {
+			alert("Guessing number is less than the correct number")
+			userValueEle.value="";
+		}
+		else {
+			alert("Guessing number is more than the correct number")	
+			userValueEle.value="";
+		}
 	}
 };
 
